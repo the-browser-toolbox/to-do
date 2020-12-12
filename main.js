fe64a2d86ab9de6376f82fde20c728bf2cbe4ffa
Anshuman Mohapatra <amohapatra2001@gmail.com>
2020-12-12 13:25:49 +0530

    logic for removing list done

--- main.js
+++ main.js
@@ -1,39 +1,50 @@
-let id   = 0
-let list = []
+let id = 0;
+let list = [];
 
-chrome.storage.sync.get('todo', storage => {
+chrome.storage.sync.get("todo", (storage) => {
   if (!storage.todo) {
-    return
+    return;
   }
 
-  list = storage.todo
-  id   = list[list.length - 1].id
+  list = storage.todo;
+  id = list[list.length - 1].id + 1;
 
-  list.map(list => addListItem(list.value))
-})
+  list.map((list) => addListItem(list.value));
+});
 
-document.querySelector('#add-task').addEventListener('click', async () => {
-  const element = document.querySelector('#task-value')
-  const value   = element.value
+document.querySelector("#add-task").addEventListener("click", async () => {
+  const element = document.querySelector("#task-value");
+  const value = element.value;
 
   if (!value) {
-    return
+    return;
   }
 
-  element.value = ''
+  element.value = "";
 
-  list.push({ id, value })
+  list.push({ id, value });
 
-  chrome.storage.sync.set({ todo: list }, () => addListItem(value))
+  chrome.storage.sync.set({ todo: list }, () => addListItem(value));
 
-  id++
-})
+  id++;
+});
+
+document.querySelector("#list").addEventListener("click", function (e) {
+  var text = e.target.textContent;
+  this.removeChild(e.target);
+  list = list.filter((item) => item.value !== text);
+  chrome.storage.sync.set({ todo: list });
+});
 
 function addListItem(value) {
-  const element = document.createElement('li')
-
-  element.classList.add('list-group-item', 'text-capitalize', 'd-flex', 'justify-content-between', 'my-2')
-  element.textContent = value
-
-  document.querySelector('#list').appendChild(element)
+  const element = document.createElement("li");
+  element.classList.add(
+    "list-group-item",
+    "text-capitalize",
+    "d-flex",
+    "justify-content-between",
+    "my-2"
+  );
+  element.textContent = value;
+  document.querySelector("#list").appendChild(element);
 }
