5ca72f152d69c913106501f29e98588d010c418e
maximilianschmelzer <mschmelzer@protonmail.ch>
2020-12-15 16:56:42 +0100

    close #8 auto fix code

--- background.js
+++ background.js
@@ -1,14 +1,14 @@
 chrome.runtime.onInstalled.addListener(() => {
-  console.log("onInstalled...");
-  // create alarm after extension is installed / upgraded
-  chrome.alarms.create("refresh", { periodInMinutes: 3 });
-});
+    console.log('onInstalled...')
+    // create alarm after extension is installed / upgraded
+    chrome.alarms.create('refresh', { periodInMinutes: 3 })
+})
 
 chrome.alarms.onAlarm.addListener((alarm) => {
-  console.log(alarm.name); // refresh
-  helloWorld();
-});
+    console.log(alarm.name) // refresh
+    helloWorld()
+})
 
 function helloWorld() {
-  console.log("Hello, world!");
+    console.log('Hello, world!')
 }
