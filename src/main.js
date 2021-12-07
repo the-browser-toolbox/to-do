ecd84353eb5d908bba1ee9b14a94684806d1a311
Mahesh Gaikwad <gaikwadmahesh777g@gmail.com>
2021-12-07 13:51:04 +0530

    Navbar added

--- src/main.js
+++ src/main.js
@@ -1,3 +1,49 @@
+
+//navbar section Starts
+
+
+window.addEventListener('resize', function(){
+    addRequiredClass();
+})
+
+
+function addRequiredClass() {
+    if (window.innerWidth < 860) {
+        document.body.classList.add('mobile')
+    } else {
+        document.body.classList.remove('mobile') 
+    }
+}
+
+window.onload = addRequiredClass
+
+let hamburger = document.querySelector('.hamburger')
+let mobileNav = document.querySelector('.nav-list')
+
+let bars = document.querySelectorAll('.hamburger span')
+
+let isActive = false
+
+hamburger.addEventListener('click', function() {
+    mobileNav.classList.toggle('open')
+    if(!isActive) {
+        bars[0].style.transform = 'rotate(45deg)'
+        bars[1].style.opacity = '0'
+        bars[2].style.transform = 'rotate(-45deg)'
+        isActive = true
+    } else {
+        bars[0].style.transform = 'rotate(0deg)'
+        bars[1].style.opacity = '1'
+        bars[2].style.transform = 'rotate(0deg)'
+        isActive = false
+    }
+    
+
+})
+
+//navbar section Ends
+
+
 let id = 0
 let list = []
 
@@ -52,3 +98,5 @@ function addListItem(value) {
     element.appendChild(span)
     document.querySelector('#list').appendChild(element)
 }
+
+
